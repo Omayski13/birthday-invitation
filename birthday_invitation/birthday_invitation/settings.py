e54e4142ea76c74e66de0f567b9af92a21e5b04f
Omayski13 <k.omayski@abv.bg>
2025-04-18 19:09:58 +0300

    fix: updated requirements.txt

--- birthday_invitation/birthday_invitation/settings.py
+++ birthday_invitation/birthday_invitation/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*u0ii#^f^xjc1^bj-l%dp+&i5ms8!wywkn8oyzc1u6k(xv=obi'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.getenv('SECRET_KEY',config('SECRET_KEY'))
+DEBUG = os.getenv('DEBUG',config('DEBUG')) == 'True'
 
 ALLOWED_HOSTS = [
     'https://7523-92-247-253-43.ngrok-free.app',
